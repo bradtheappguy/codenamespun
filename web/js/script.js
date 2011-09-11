4b82925b385c07e3ab8df9a138e6344bc3787ba9
Justin Mahon <justinmahon@gmail.com>
2011-09-10 19:36:20 -0700

    skeleton for webview

--- web/js/script.js
+++ web/js/script.js
@@ -1,1 +1,14 @@
 /* script.js */
+
+function revealFromTop(target) {
+	$(target).animate({
+		top:'0'
+	}, 200);
+}
+
+function hideFromTop(target) {
+	$(target).animate({
+		opacity:0,
+		top:'-100%'
+	}, 200);
+}
