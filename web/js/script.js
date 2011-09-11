63fb9a8176b712e99a170b081a43b1d0deab49bc
Brad Smith <bradsmithinc@gmail.com>
2011-09-10 21:17:28 -0700

    ajx home page

--- web/js/script.js
+++ web/js/script.js
@@ -16,10 +16,14 @@ function hideFromTop(target) {
 $(document).ready(function () {
   $.getJSON('http://spunapi.herokuapp.com/feed.json', function(data) {
             var items = [];
-            $.each(data, function(key, val) {
-                   $.each(val, function(key, val) {
-                          alert(key + val);
-                   });
+            $.each(data, function(key, val) {                  
+                   var li = "<li>";;
+                   li += '<img src="' + val['avatar'] + '">';
+                   li += '<span class="notification">' + val['status'] + '</span>';
+                   li += '<a>Add</a>';
+                   li += '<a>Play</a>';
+                   li += "</li>";
+                   $('#activity .table-view').append(li)
             });
   });               
 });
